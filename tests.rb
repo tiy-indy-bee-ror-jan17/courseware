8ff9252618a70151499cc418c78ef9d4eff52249
Michael Stashevsky <mstashev@gmail.com>
2017-02-16 13:02:46 -0500

    updated Gemfile and tests.rb

--- tests.rb
+++ tests.rb
@@ -20,9 +20,63 @@ ApplicationMigration.migrate(:up)
 
 # Finally!  Let's test the thing.
 class ApplicationTest < Minitest::Test
+  include MiniTest::ActiveRecordAssertions
 
   def test_truth
     assert true
   end
 
+  
+
+
+
+
+
+
+
+
+
+
 end
+
+# Person A
+# Associate schools with terms (both directions).
+# Associate terms with courses (both directions). If a term has any courses associated with it, the term should not be deletable.
+# Associate courses with course_students (both directions). If the course has any students associated with it, the course should not be deletable.
+# Associate assignments with courses (both directions). When a course is destroyed, its assignments should be automatically destroyed.
+# Associate lessons with their pre_class_assignments (both directions).
+# Set up a School to have many courses through the school's terms.
+# Validate that Lessons have names.
+# Validate that Readings must have an order_number, a lesson_id, and a url.
+# Validate that the Readings url must start with http:// or https://. Use a regular expression.
+# Validate that Courses have a course_code and a name.
+# Validate that the course_code is unique within a given term_id.
+# Validate that the course_code starts with three letters and ends with three numbers. Use a regular expression.
+# Associate course_instructors with instructors (who happen to be users)
+# Associate assignments with assignment_grades (both directions)
+# Set up a Course to have many instructors through the Course's course_instructors.
+# Validate that an Assignment's due_at field is not before the Assignment's active_at.
+# A Course's assignments should be ordered by due_at, then active_at.
+#
+# Person B
+# Associate lessons with readings (both directions). When a lesson is destroyed, its readings should be automatically destroyed.
+# Associate lessons with courses (both directions). When a course is destroyed, its lessons should be automatically destroyed.
+# Associate courses with course_instructors (both directions). If the course has any instructors associated with it, the course should not be deletable.
+# Associate lessons with their in_class_assignments (both directions).
+# Set up a Course to have many readings through the Course's lessons.
+# Validate that Schools must have name.
+# Validate that Terms must have name, starts_on, ends_on, and school_id.
+# Validate that the User has a first_name, a last_name, and an email.
+# Validate that the User's email is unique.
+# Validate that the User's email has the appropriate form for an e-mail address. Use a regular expression.
+# Validate that the User's photo_url must start with http:// or https://. Use a regular expression.
+# Validate that Assignments have a course_id, name, and percent_of_grade.
+# Validate that the Assignment name is unique within a given course_id.
+# Associate CourseStudents with students (who happen to be users)
+# Associate CourseStudents with assignment_grades (both directions)
+# Set up a Course to have many students through the course's course_students.
+# Associate a Course with its ONE primary_instructor. This primary instructor is the one who is referenced by a course_instructor which has its primary flag set to true.
+# A Course's students should be ordered by last_name, then first_name.
+#
+# Together
+# Associate Lessons with their child_lessons (and vice-versa). Sort the child_lessons by id.
