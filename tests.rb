34240d83be312879b82de4588abe2c818da8a335
CosmicCandi <kalea.wolff@gmail.com>
2017-02-17 15:02:05 -0500

    Most of the way through explorer mode. Finished through 'user email address is unique'.

--- tests.rb
+++ tests.rb
@@ -65,6 +65,88 @@ class ApplicationTest < Minitest::Test
     assert lesson_test.in_class_assignment_id == assign_test.id
   end
 
+  #B-Test-5
+  def test_a_course_has_many_readings_through_lessons
+    course_many_readings_test = Course.create(name: "Advanced Lesson Destroying")
+    lesson_test = Lesson.create(course_id: course_many_readings_test.id, name: "Lesson Destroying Best Practices")
+    lesson_test2 = Lesson.create(course_id: course_many_readings_test.id, name: "Lesson Destroying: Safety")
+    reading_test1 = Reading.create(lesson_id: lesson_test.id, caption: "Lesson Destroying: Industry Methods and Standards")
+    reading_test2 = Reading.create(lesson_id: lesson_test.id, caption: "How to destroy Lessons Safely")
+    reading_test3 = Reading.create(lesson_id: lesson_test2.id, caption: "What to do after you've destroyed a lesson")
+
+    assert course_many_readings_test.readings.count > 2
+  end
+
+  #B-Test-6
+  def test_validate_a_school_has_a_name
+    new_school = School.create()
+    assert new_school.name == nil
+    assert new_school.errors.messages
+    refute new_school.save
+  end
+
+  #The following tests come from a single deliverable.
+  #B-Test-7
+  #Date (used later) requires YYYY-MM-DD format
+  def test_validate_terms_must_have_a_name
+    terms_have_names = Term.new()
+    assert terms_have_names.name == nil
+    assert terms_have_names.errors.messages
+    refute terms_have_names.save
+  end
+
+  def test_validate_terms_must_have_starts_on
+    terms_have_starts_on = Term.new(name: "Winter")
+    assert terms_have_starts_on.starts_on == nil
+    assert terms_have_starts_on.errors.messages
+    refute terms_have_starts_on.save
+  end
+
+  def test_validate_terms_must_have_ends_on
+    terms_have_ends_on = Term.new(name: "Spring", starts_on: "2017-02-16")
+    assert terms_have_ends_on.ends_on == nil
+    assert terms_have_ends_on.errors.messages
+    refute terms_have_ends_on.save
+  end
+
+  def test_validate_terms_must_have_a_school_id
+    terms_have_school_id = Term.new(name: "Summer", starts_on: Date.today, ends_on: "2017-04-29")
+    assert terms_have_school_id.school_id == nil
+    assert terms_have_school_id.errors.messages
+    refute terms_have_school_id.save
+  end
+
+  #B-Test-8
+  #The following tests come from a single deliverable
+  def test_that_a_user_has_a_first_name
+    user_first_name = User.new()
+    assert user_first_name.first_name == nil
+    assert user_first_name.errors.messages
+    refute user_first_name.save
+  end
+
+  def test_that_a_user_has_a_last_name
+    user_last_name = User.new(first_name: "Bobby")
+    assert user_last_name.last_name == nil
+    assert user_last_name.errors.messages
+    refute user_last_name.save
+  end
+
+  def test_that_a_user_has_an_email
+    user_email = User.new(first_name: "Bobby", last_name: "Tables")
+    assert user_email.email == ""
+    assert user_email.errors.messages
+    refute user_email.save
+  end
+
+  #B-Test-9
+  def test_that_a_users_email_is_unique
+    unique_email = User.new(first_name: "Bobby", last_name: "Tables", email: "dropallthetables@dropitlikeitshot.com")
+    assert unique_email.save
+    unique_email1 = User.new(first_name: "Fred", last_name: "Dunston", email: "dropallthetables@dropitlikeitshot.com")
+    assert unique_email1.errors.messages
+    refute unique_email1.save
+  end
 
 
 #End of Class
