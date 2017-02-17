3dcd7ce3e3cd33a7f265f1439ea217c19e20c5d0
SoriaMiguel <soria.miguel.8@gmail.com>
2017-02-17 15:13:03 -0500

    fixed failures in tests

--- assignment.rb
+++ assignment.rb
@@ -2,7 +2,7 @@ class Assignment < ActiveRecord::Base
 
   belongs_to    :course
   has_many      :lessons, foreign_key: 'pre_class_assignment_id'
-  has_many      :lessons, foreign_key: 'in_class_assignment_id'
+  has_many      :class_lessons, foreign_key: 'in_class_assignment_id', class_name: "Lesson"
 
   scope :active_for_students, -> { where("active_at <= ? AND due_at >= ? AND students_can_submit = ?", Time.now, Time.now, true) }
 
