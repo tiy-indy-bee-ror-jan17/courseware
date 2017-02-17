976ba5cbadedcd6ca0b6dad64f4449f677fec126
DAVMCKIN <DAVIDJMCKIN@GMAIL.COM>
2017-02-17 13:37:36 -0500

    moar stuff

--- reading.rb
+++ reading.rb
@@ -6,7 +6,27 @@ class Reading < ActiveRecord::Base
   scope :pre, -> { where("before_lesson = ?", true) }
   scope :post, -> { where("before_lesson != ?", true) }
 
+  validates :order_number, presence: true
+  validates :lesson_id, presence: true
+  validates :url, presence: true
+
   def clone
     dup
   end
+
+
+#   def url_checker
+#     url.start_with?("http://") || url.start_with?("https://")
+#   end
+# end
+
+# def url_validator(url)
+#
+# url_regex = Regexp.new("((https?|ftp|file):((//)|(\\\\))+[\w\d:\#@%/;$()~_?\+-=\\\\.&]*)")
+#
+#     if url =~ url_regex then
+#         puts "%s is valid" % url
+#     else
+#         puts "%s not valid" % url
+#     end
 end
