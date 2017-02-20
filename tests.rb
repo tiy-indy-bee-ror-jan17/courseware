b16f97041e58b2f3f8155367ce87ff8f996868b9
Michael Stashevsky <mstashev@gmail.com>
2017-02-20 08:41:35 -0500

    Commit

--- tests.rb
+++ tests.rb
@@ -35,8 +35,6 @@ class ApplicationTest < Minitest::Test
     assert school.respond_to?(:terms)
   end
 
-
-
   def test_term_can_be_added_to_school
     school = School.create(name: "NSES")
     Term.create(name: "Fall",
@@ -63,21 +61,24 @@ class ApplicationTest < Minitest::Test
                        ends_on: "01-02-0001",
                        school_id: 1)
     assert term.save
-    course = Course.create(name: "Math", term_id: term.id)
+    course = Course.create(name: "Math",
+                           term_id: term.id)
     assert course.save
     assert course.term.name == "Fall"
   end
 
   def test_courses_with_course_students_association
     user = User.create(first_name: "Test",
-                      last_name: "User",
-                      email: "user2@te2sting.com")
+                       last_name: "User",
+                       email: "user2@te2sting.com")
     course = Course.create(name: "Math")
     # p user
     # puts "\n"
     # p course
     # puts "\n"
-    course_student = CourseStudent.create(course_id: course.id, student_id: user.id)
+    course_student = CourseStudent.create(
+                                    course_id: course.id,
+                                    student_id: user.id)
     # p course_student
     assert course_student.course_id == course.id
     refute course.destroy
@@ -106,7 +107,7 @@ class ApplicationTest < Minitest::Test
   end
 
   def test_lessons_readings_association
-    lesson = Lesson.create()
+    lesson = Lesson.create(name: "Test Me MoFo")
     reading = Reading.create(lesson_id: lesson.id)
     # p reading
     # p lesson
@@ -121,7 +122,8 @@ class ApplicationTest < Minitest::Test
 
   def test_lessons_courses_association
     course = Course.create()
-    lesson = Lesson.create(course_id: course.id)
+    lesson = Lesson.create(course_id: course.id,
+                           name: "Tesy MoFo")
     # test1 = course
     # test2 = lesson
     # p test1
@@ -135,9 +137,11 @@ class ApplicationTest < Minitest::Test
                        email: "test@used.com",
                        instructor: true)
     course = Course.create()
-    course_instructor = CourseInstructor.create(
-                                          course_id: course.id,
-                                          instructor_id: user.id)
+    course_instructor =
+            CourseInstructor.create(
+                                    course_id: course.id,
+                                    instructor_id: user.id
+                                   )
     # p course
     # p course_instructor
     # puts "\n"
@@ -158,6 +162,7 @@ class ApplicationTest < Minitest::Test
                           percent_of_grade: 100.00
                           )
     lesson_assignment = Lesson.create(
+                          name: "That's the name of the game",
                           in_class_assignment_id: assignment.id
                         )
     # puts "In Class Assignment\n"
@@ -167,8 +172,7 @@ class ApplicationTest < Minitest::Test
     # puts "\n"
     # puts "================================================="
     # puts "\n"
-    # assert_equal assignment.id, lesson_assignment
-                    # .in_class_assignment_id
+    # assert_equal assignment.id, lesson_assignment.in_class_assignment_id
     assert lesson_assignment.in_class_assignments == assignment
   end
 
@@ -179,9 +183,11 @@ class ApplicationTest < Minitest::Test
                           course_id: course.id,
                           percent_of_grade: 100.00
                           )
-    lesson_assignments = Lesson.create(
-                            pre_class_assignment_id: assignments.id
-                          )
+    lesson_assignments =
+          Lesson.create(
+                        name: "2 Infinity and Beyond!!!!!!!!!!!",
+                        pre_class_assignment_id: assignments.id
+                       )
     # puts "Pre Class Assignment\n"
     # puts "\n"
     # p lesson_assignments
@@ -190,24 +196,22 @@ class ApplicationTest < Minitest::Test
     # puts "\n"
     # puts "=============================================="
     # puts "\n"
-    # assert_equal assignments.id,
-                    # lesson_assignments.pre_class_assignment_id
-    assert lesson_assignments.pre_class_assignments == assignments
+    # assert_equal assignments.id, lesson_assignments.pre_class_assignment_id
+    assert lesson_assignments
+              .pre_class_assignments == assignments
   end
 
   def test_course_readings_association
     course = Course.create()
-    lesson = Lesson.create(course_id: course.id)
-    # reading = Reading.create(lesson_id: lesson.id)
-
+    lesson = Lesson.create(
+                           name: "",
+                           course_id: course.id
+                          )
     assert affirm(course.lessons, 'Lesson')
     assert affirm(lesson.readings, 'Reading')
     assert affirm(course.readings, 'Reading')
   end
-    # Validate that Readings must have an order_number, a lesson_id,
-    #  and a url.
-    #
-
+    # Validate that Readings must have an order_number, a lesson_id, and a url.
   def test_url_verification
 
   end
@@ -216,9 +220,7 @@ class ApplicationTest < Minitest::Test
     school = School.create(name: "")
     refute school.save
     # p school.errors.full_messages
-    # assert school.errors.full_messages
-                # .include?("Name can't be blank"),
-                # school.errors.full_messages
+    # assert school.errors.full_messages.include?("Name can't be blank"), school.errors.full_messages
   end
 
   def test_terms_have_name_starts_on_ends_on_school_id
@@ -228,21 +230,12 @@ class ApplicationTest < Minitest::Test
                        school_id: "")
     refute term.save
     # p term.errors.full_messages
-    # assert term.errors.full_messages
-              # .include?("Name can't be blank"),
-              #   term.errors.full_messages
-    # assert term.errors.full_messages
-              # .include?("Starts on can't be blank"),
-              #   term.errors.full_messages
-    # assert term.errors.full_messages
-              # .include?("Ends on can't be blank"),
-              #   term.errors.full_messages
-    # assert term.errors.full_messages
-              # .include?("School id can't be blank"),
-              #   term.errors.full_messages
+    # assert term.errors.full_messages.include?("Name can't be blank"), term.errors.full_messages
+    # assert term.errors.full_messages.include?("Starts on can't be blank"), term.errors.full_messages
+    # assert term.errors.full_messages.include?("Ends on can't be blank"), term.errors.full_messages
+    # assert term.errors.full_messages.include?("School id can't be blank"), term.errors.full_messages
   end
-    # Validate that the Readings url must start with http:// or
-    # https://. Use a regular expression.
+    # Validate that the Readings url must start with http:// or  https://. Use a regular expression.
     #
 
   def test_courses_have_course_code_and_name
@@ -252,15 +245,9 @@ class ApplicationTest < Minitest::Test
     user = User.create(first_name: "", last_name: "", email: "")
     refute user.save
     # p user.errors.full_messages
-    # assert user.errors.full_messages
-              # .include?("First name can't be blank"),
-              #   user.errors.full_messages
-    # assert user.errors.full_messages
-              # .include?("Last name can't be blank"),
-              #   user.errors.full_messages
-    # assert user.errors.full_messages
-              # .include?("Email can't be blank"),
-              #   user.errors.full_messages
+    # assert user.errors.full_messages.include?("First name can't be blank"), user.errors.full_messages
+    # assert user.errors.full_messages.include?("Last name can't be blank"), user.errors.full_messages
+    # assert user.errors.full_messages.include?("Email can't be blank"), user.errors.full_messages
   end
 
   def test_user_email_unique
@@ -274,9 +261,7 @@ class ApplicationTest < Minitest::Test
                         email: "test@user.com")
     refute userB.save
     # p userB.errors.full_messages
-    # assert userB.errors.full_messages
-                # .include?("Email has already been taken"),
-                #   userB.errors.full_messages
+    # assert userB.errors.full_messages.include?("Email has already been taken"), userB.errors.full_messages
   end
     # Validate that Courses have a course_code and a name.
 
@@ -316,15 +301,9 @@ class ApplicationTest < Minitest::Test
                                 percent_of_grade: 0)
     refute assignment.save
     # p assignment.errors.full_messages
-    # assert assignment.errors.full_messages
-              # .include?("Course id can't be blank"),
-                # assignment.errors.full_messages
-    # assert assignment.errors.full_messages
-              # .include?("Name can't be blank"),
-              #   assignment.errors.full_messages
-    # assert assignment.errors.full_messages
-              # .include?("Percent of grade can't be blank"),
-              #   assignment.errors.full_messages
+    # assert assignment.errors.full_messages.include?("Course id can't be blank"), assignment.errors.full_messages
+    # assert assignment.errors.full_messages.include?("Name can't be blank"), assignment.errors.full_messages
+    # assert assignment.errors.full_messages.include?("Percent of grade can't be blank"), assignment.errors.full_messages
   end
 
   def test_assignment_name_unique_within_course
@@ -338,164 +317,15 @@ class ApplicationTest < Minitest::Test
     assert assignment.save
     refute assignment2.save
   end
-    # Validate that the course_code starts with three letters and ends
-    # with three numbers. Use a regular expression.
+    # Validate that the course_code starts with three letters and end with three numbers. Use a regular expression.
     #
-    # Associate course_instructors with instructors (who happen to be
-    # users)
+    # Associate course_instructors with instructors (who happen to be users)
     #
-    # Associate assignments with assignment_grades (both directions)
-    # Set up a Course to have many instructors through the Course's
-    # course_instructors.
+    # Associate assignments with assignment_grades (both directions) Set up a Course to have many instructors through the Course's course_instructors.
     #
-    # Validate that an Assignment's due_at field is not before the
-    # Assignment's active_at.
+    # Validate that an Assignment's due_at field is not before the Assignment's active_at.
     #
-    # A Course's assignments should be ordered by due_at, then
-    # active_at.
-  #
-  # Together
-  # Associate Lessons with their child_lessons (and vice-versa). Sort
-  # the child_lessons by id.
-
-
-
-
-
-
-#///////////////////////////////////////////////////////////
-  # def test_lessons_readings_association
-  #   assert_association Lesson, :has_many, :readings
-  #   assert_association Reading, :belongs_to, :lesson
-  # end
-  #
-  # def test_lessons_courses_association
-  #   assert_association Course, :has_many, :lessons
-  #   assert_association Lesson, :belongs_to, :course
-  # end
-  #
-  # def test_courses_instructors_association
-  #   assert_association Course, :has_many, :instructors
-  #   assert_association CourseInstructor, :belongs_to, :course
-  # end
-  #
-  # def test_lessons_in_class_assignments_association
-  #   assert_association Lesson, :has_many, :in_class_assignments
-  #   assert_association Assignment, :belongs_to, :lesson
-  # end
-  #
-  # def test_course_readings_association
-  #   assert_association Course, :has_and_belongs_to_many, :readings
-  #   assert_association Reading, :has_and_belongs_to_many, :courses
-  # end
-  #
-  # def test_school_has_name
-  #   # Validate that Schools must have name.
-  # end
-  #
-  # def test_terms_have_name_starts_on_ends_on_school_id
-  #   # Validate that Terms must have name, starts_on, ends_on, and
-  #   # school_id.
-  # end
-  #
-  # def test_user_has_name_and_email
-  #   # Validate that the User has a first_name, a last_name, and an
-  #   # email.
-  # end
-  #
-  # def test_user_email_unique
-  #   # Validate that the User's email is unique.
-  # end
-  #
-  # def test_user_email_proper_format
-  #   # Validate that the User's email has the appropriate form for
-  #   # an e-mail address. Use a regular expression.
-  # end
-  #
-  # def test_user_photo_url
-  #   # Validate that the User's photo_url must start with http://
-  #   # or https://. Use a regular expression.
-  # end
-  #
-  # def test_assignments_have_course_id_name_percent
-  #   # Validate that Assignments have a course_id, name, and
-  #   # percent_of_grade.
-  # end
-  #
-  # def test_assignment_name_unique_within_course
-  #   # Validate that the Assignment name is unique within a given
-  #   # course_id.
-  # end
-  #
-  # def test_coursestudents_students_association
-  #   assert_association CourseStudent, :belongs_to, :student
-  #   assert_association User, :has_many, :course_students
-  # end
-  #
-  # def test_coursestudents_assignment_grades_association
-  #   assert_association CourseStudent, :has_many, :assignment_grades
-  #   assert_association AssignmentGrade, :belongs_to, :course_student
-  # end
-  #
-  # def test_course_has_many_students_through_course_student
-  #   # Set up a Course to have many students through the course's
-  #   # course_students.
-  # end
-  #
-  # def test_course_primary_instructor_association
-  #   # Associate a Course with its ONE primary_instructor. This
-  #   # primary instructor is the one who  is referenced by a
-  #   # course_instructor which has its primary flag set to true.
-  # end
-  #
-  # def test_course_students_ordered_last_first_name
-  #   # A Course's students should be ordered by last_name, then
-  #   # first_name.
-  # end
-
-
-#///////////////////////////////////////////////////////////////////////////
-# Person A
-  # Associate schools with terms (both directions).
-  # Associate terms with courses (both directions). If a term has any
-  # courses associated with it, the term should not be deletable.
-  #
-  # Associate courses with course_students (both directions). If the
-  # course has any students associated with it, the course should not
-  # be deletable.
-  #
-  # Associate assignments with courses (both directions). When a
-  # course is destroyed, its assignments should be automatically
-  # destroyed.
-  #
-  # Associate lessons with their pre_class_assignments (both
-  # directions)
-  #
-  # Set up a School to have many courses through the school's terms.
-  # Validate that Lessons have names.
-  # Validate that Readings must have an order_number, a lesson_id,
-  #  and a url.
-  #
-  # Validate that the Readings url must start with http:// or
-  # https://. Use a regular expression.
-  #
-  # Validate that Courses have a course_code and a name.
-  # Validate that the course_code is unique within a given term_id.
-  # Validate that the course_code starts with three letters and ends
-  # with three numbers. Use a regular expression.
-  #
-  # Associate course_instructors with instructors (who happen to be
-  # users)
-  #
-  # Associate assignments with assignment_grades (both directions)
-  # Set up a Course to have many instructors through the Course's
-  # course_instructors.
-  #
-  # Validate that an Assignment's due_at field is not before the
-  # Assignment's active_at.
-  #
-  # A Course's assignments should be ordered by due_at, then
-  # active_at.
+    # A Course's assignments should be ordered by due_at, then active_at.
 
   def test_coursestudents_students_association
     user = User.create(first_name: "Test",
@@ -519,8 +349,9 @@ class ApplicationTest < Minitest::Test
     # p course_student
     # puts "\n"
     # p assignment_grade
-    # puts "======================================================"
-    assert affirm(assignment_grade.course_student, 'CourseStudent')
+    # puts "==================================================="
+    assert affirm(assignment_grade.course_student,
+                  'CourseStudent')
   end
 
   def test_course_has_many_students_through_course_student
@@ -564,13 +395,21 @@ class ApplicationTest < Minitest::Test
            course_instructor.course_id == course.id
   end
 
-  def test_course_students_ordered_last_first_name
-    # A Course's students should be ordered by last_name, then
-    # first_name.
-  end
+  def test_sorting_last_and_first_names_by_student
+    student = User.create(first_name: "Michael",last_name: "Stashevsky",email: "mike@stashevsky.com")
+    student2 = User.create(first_name: "Stephen",last_name: "Stashevsky",email: "stephen@stashevsky.com")
+    student3 = User.create(first_name: "David",last_name: "Stashevsky",email: "dave@stashevsky.com")
+    student4 = User.create(first_name: "Jennifer",last_name: "Stashevsky",email: "jen@stashevsky.com")
+    student5 = User.create(first_name: "Andrew",last_name: "Evan",email: "andrew@evan.com")
+    student6 = User.create(first_name: "Bonnie",last_name: "Evan",email: "bonnie@evan.com")
+    student7 = User.create(first_name: "Paul",last_name: "Evan", email: "paul@evan.com")
+    student8 = User.create(first_name: "Ben",last_name: "Evan",email: "ben@evan.com")
+    student9 = User.create(first_name: "Aimee", last_name: "Evan", email: "aimee@evan.com")
+    student10 = User.create(first_name: "Sam", last_name: "Evan", email: "sam@evan.com")
 
+    binding.pry
+
+
+
+  end
 end
-#
-# Together
-# Associate Lessons with their child_lessons (and vice-versa). Sort
-# the child_lessons by id
