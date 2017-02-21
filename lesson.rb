7f893ab37f4695cbaba3004efb990d882315830d
CosmicCandi <kalea.wolff@gmail.com>
2017-02-20 20:35:08 -0500

    Finished epic mode.  Adds in an assertion that a child lesson is sorted by its id.

--- lesson.rb
+++ lesson.rb
@@ -4,6 +4,8 @@ class Lesson < ActiveRecord::Base
   belongs_to :in_class_assignment, class_name: "Assignment", foreign_key: "in_class_assignment_id"
   belongs_to :course
   has_many :readings, dependent: :destroy
+  belongs_to :parent_lesson, class_name: "Lesson", foreign_key: "parent_lesson_id"
+  has_many :child_lessons, class_name: "Lesson", foreign_key: "parent_lesson_id"
 
   delegate :code_and_name, to: :course, prefix: true
 
